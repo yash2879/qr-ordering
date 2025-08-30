2477d0c4f17ddb64d1eb274e87f51a53eefd5f63
Yash Dharmale <yashd2879@gmail.com>
2025-08-30 17:28:53 +0530

    feat: Initialize tablese-core project structure - Spring boot

---
+++ src/main/java/in/tablese/tablese_core/TableseCoreApplication.java
@@ -0,0 +1,13 @@
+package in.tablese.tablese_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TableseCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TableseCoreApplication.class, args);
+	}
+
+}
